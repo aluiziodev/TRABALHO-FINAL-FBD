e4b25750a59ba4dd67175f4e5b022759b535146b
Aluizio Almendra <aluneto2000@gmail.com>
2026-01-16 16:43:45 +0000

    Add files via upload

---
+++ DATABASE/04_function.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION function_albuns_compositor 
+( @nome_comp VARCHAR(100) )
+
+RETURNS TABLE 
+AS RETURN
+( SELECT a.cod_alb, a.descricao FROM compositor c JOIN 
+compositor_faixa cf ON c.cod_comp=cf.cod_comp JOIN faixa f ON (f.num_faixa_alb=cf.num_faixa_alb
+AND f.alb_faixa=cf.alb_faixa) JOIN album a ON a.cod_alb=f.alb_faixa
+WHERE c.nome LIKE '%' + @nome_comp + '%');
+
---
+++ DATABASE/05_indices.sql
@@ -0,0 +1,10 @@
+CREATE NONCLUSTERED INDEX idx_faixa_album
+ON faixa (alb_faixa)
+WITH (FILLFACTOR = 100)
+ON FG_PLAYLISTS;
+
+
+CREATE NONCLUSTERED INDEX idx_faixa_tipo_comp
+ON faixa (tipo_comp)
+WITH (FILLFACTOR = 100)
+ON FG_PLAYLISTS;
